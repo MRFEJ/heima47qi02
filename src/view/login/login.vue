8703404b7ea9ea53f3994f0ce8fc75669874b1c4
MRFEJ <1403676630@qq.comgit config --global user.name Your>
2020-02-21 23:03:49 +0800

    完成登录页面的基本布局和表单验证

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -1,12 +1,106 @@
 <template>
   <div class="login">
-    <div class="login-left"></div>
+    <div class="login_left">
+      <div class="login_title">
+        <img class="login_logo" src="./img/login_logo.png" alt />
+        <span class="l_text">黑马面面</span>
+        <span class="l_s"></span>
+        <span class="l_text">用户登录</span>
+      </div>
+      <!-- 表单 -->
+      <el-form :model="form" :rules="rules" ref="form" class="demo-ruleForm">
+        <el-form-item prop="phone">
+          <el-input clearable prefix-icon="el-icon-user" v-model="form.phone" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            clearable
+            show-password
+            prefix-icon="el-icon-lock"
+            v-model="form.password"
+            placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item prop="code">
+          <el-row>
+            <el-col :span="17">
+              <el-input
+                clearable
+                prefix-icon="el-icon-key"
+                v-model="form.code"
+                placeholder="请输入验证码"
+              ></el-input>
+            </el-col>
+            <el-col :span="7">
+              <img class="img_code" src="./img/login_banner_ele.png" alt />
+            </el-col>
+          </el-row>
+        </el-form-item>
+
+        <el-form-item prop="checked">
+          <el-checkbox v-model="form.checked">
+            我已阅读并同意
+            <el-link type="primary">用户协议</el-link>和
+            <el-link type="primary">隐私条款</el-link>
+          </el-checkbox>
+        </el-form-item>
+
+        <el-form-item>
+          <el-row>
+            <el-col>
+              <el-button type="primary" @click="submitForm">立即创建</el-button>
+              <el-button type="primary">注册</el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+      </el-form>
+    </div>
     <img src="./img/login_banner_ele.png" alt />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      form: {
+        phone: "",
+        password: "",
+        code: "",
+        checked: false
+      },
+      rules: {
+        phone: [
+          { required: true, message: "手机号不能为空", trigger: "blur" },
+          { pattern: /0?(13|14|15|18)[0-9]{9}/, message: "手机号格式不正确",trigger:'blur' }
+        ],
+
+        password: [
+          { required: true, message: "密码不能为空", trigger: "blur" },
+          { max: 12, min: 6, message: "密码在6-12位", trigger: "change" }
+        ],
+        code: [{ required: true, message: "验证码不能为空", trigger: "blur" }],
+        checked: [
+          {
+            pattern: /true/,
+            message: "请勾选我已阅读并同意用户协议和隐私条款",
+            trigger: "change"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm() {
+      this.$refs.form.validate(v => {
+        if (v) {
+          alert("登录成功");
+        }
+      });
+    }
+  }
+};
 </script>
 
 <style lang="less">
@@ -21,10 +115,43 @@ export default {};
   display: flex;
   justify-content: space-around;
   align-items: center;
-  .login-left {
+  .login_left {
     width: 478px;
     height: 550px;
     background: rgba(245, 245, 245, 1);
+    padding: 43px;
+    .login_title {
+      display: flex;
+      align-items: center;
+      margin-bottom: 29px;
+      .login_logo {
+        width: 22px;
+        height: 17px;
+        margin-right: 16px;
+      }
+      .l_s {
+        width: 1px;
+        height: 28px;
+        background: rgba(199, 199, 199, 1);
+        margin-left: 14px;
+        margin-right: 12px;
+      }
+      .l_text {
+        font-size: 24px;
+        color: rgba(12, 12, 12, 1);
+      }
+    }
+    .el-button {
+      width: 100%;
+      &:nth-child(2) {
+        margin-left: 0;
+        margin-top: 26px;
+      }
+    }
+    .img_code {
+      width: 100%;
+      height: 40px;
+    }
   }
 }
 html,
