2a3c208761d394238feca84368731937b8b772db
MRFEJ <1403676630@qq.comgit config --global user.name Your>
2020-02-21 22:00:13 +0800

    完成登录页面的基本布局,饿了么组件的导入,less的引入

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -9,7 +9,7 @@
 export default {};
 </script>
 
-<style>
+<style lang="less">
 .login {
   width: 100%;
   height: 100%;
@@ -18,11 +18,14 @@ export default {};
     rgba(20, 147, 250, 1),
     rgba(1, 198, 250, 1)
   );
-}
-.login-left {
-  width: 478px;
-  height: 550px;
-  background: rgba(245, 245, 245, 1);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  .login-left {
+    width: 478px;
+    height: 550px;
+    background: rgba(245, 245, 245, 1);
+  }
 }
 html,
 body {
