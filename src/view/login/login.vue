729a2356c7ff1f833650753f97e60e996900fe04
MRFEJ <1403676630@qq.comgit config --global user.name Your>
2020-02-21 21:52:37 +0800

    完成登录页面相关文件的创建

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -1,13 +1,31 @@
 <template>
-  
+  <div class="login">
+    <div class="login-left"></div>
+    <img src="./img/login_banner_ele.png" alt />
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+.login {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    225deg,
+    rgba(20, 147, 250, 1),
+    rgba(1, 198, 250, 1)
+  );
+}
+.login-left {
+  width: 478px;
+  height: 550px;
+  background: rgba(245, 245, 245, 1);
+}
+html,
+body {
+  height: 100%;
+}
 </style>
